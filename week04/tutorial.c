0c0b0826d139dfc1dcfe1d9a358b81a4a251c3a7
DebraChen <y.chen0320214@arts.ac.uk>
2021-11-16 23:31:38 +0000

    16/11/2021

--- week04/tutorial.c
+++ week04/tutorial.c
@@ -1,21 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 
 
 void main(void)
 {
-    int a = 1;
-    int b = 2;
-    int c;
-    c = a + b;
+    bool a = 1;
+    bool b = false;
+    int c = 0;
+    float var = 1.234567;
+    char word[10] = ' Debra';
+
+    if (a == 1 & b == 1)
+    {
+        c = 1;
+        // %d stands for integer value (int)
+        printf("c is equal to %d\n", c);
+    }
+    else 
+    {
+        c = 0;
+        printf("c is equal to %d\n", c);
+    }
+
     //char var[100];
     //printf("Please enter your name: ");
     //scanf("%s", var);
     //printf("Hello World!\n");
     //printf("Your name is %s\n", var);
-    printf("HELLOW WORLD \n");
-    printf("I add %d and %d equals to %d \n", a, b, c);
+    //printf("\n");
+    //printf("I add %d and %d equals to %d \n", a, b, c);
 
 }
+
+/*
+c programming
+
+data types
+- int -> integer 
+- char -> characters
+- long, short -> strings
+- bool -> boolean (true or false | 0 or 1)
+
+
+boolean algebra 
+n = AND (intersect)
+u = OR (union)
+
+A = 1
+B = 1 
+A n B = 1
+
+A = 1
+B = 0
+A n B = 0
+
+bool A = 0, B = 1;
+//when both A and B is set to 1 then return C is 1
+if (A == 1 & B == 1)
+{
+    C = 1
+}
+else 
+{
+    C = 0 
+}
+
+
+*/
